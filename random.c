91575d493a68dbce7ec59c1064f869bcd4274d4d
Tomas Chaparro <tomaschaparroossa@gmail.com>
2025-08-30 15:49:02 -0400

    Add randchar function and complete print_random program

---
+++ random.c
@@ -0,0 +1,13 @@
+// Name & class section: Tomas Chaparro, CIS 3207 section 3
+// Last modification: 8/30/2025
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+char randchar();
+
+char randchar() {
+    char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    return alphabet[rand()%26];
+}
